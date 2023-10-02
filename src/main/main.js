954b0b0a0611d7da47693cdb8661cdbc92217310
Sandeep Vemula <vsandeep414@gmail.com>
2023-10-02 21:33:41 +0100

    adding autoupdater

--- src/main/main.js
+++ src/main/main.js
@@ -8,10 +8,34 @@ if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
-const server = ''
+const server = 'https://hazel-pink-six.vercel.app'
 const url = `${server}/update/${process.platform}/${app.getVersion()}`
 
 autoUpdater.setFeedURL({ url })
+
+setInterval(() => {
+  autoUpdater.checkForUpdates()
+}, 60000)
+
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail:
+      'A new version has been downloaded. Restart the application to apply the updates.'
+  }
+
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
+
+autoUpdater.on('error', (message) => {
+  log.error('There was a problem updating the application')
+  log.error(message)
+})
 
 const createWindow = () => {
   // Create the browser window.
