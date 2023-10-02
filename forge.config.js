22ae7cb8bb869cdd2708f8926bd4f77ef0faf5f5
Sandeep Vemula <vsandeep414@gmail.com>
2023-10-02 20:13:37 +0100

    adding github actions

--- forge.config.js
+++ forge.config.js
@@ -21,6 +21,18 @@ module.exports = {
       config: {},
     },
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'bittu',
+          name: 'yt-short-studio'
+        },
+        prerelease: true
+      }
+    }
+  ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
